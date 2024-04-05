8904fa0e64123563fbfb3d53b6913dd049bbaf5e
nhutlam1705 <nhutlam1705@gmail.com>
2024-04-05 14:08:43 +0700

    Tao trang them moi mot lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="page">
+        <h4>Thêm liên hệ mới</h4>
+        <ContactForm
+            :contact="contact"
+            @submit:contact="updateContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    import ContactForm from "@/components/ContactForm.vue";
+    import ContactService from "@/services/contact.service";
+
+    export default {
+        components: {
+            ContactForm,
+        },
+        data() {
+            return {
+                contact: {},
+                message: "",
+            };
+        },
+        methods: {
+            async updateContact(data){
+                try{
+                    await ContactService.create(data);
+                    this.message = "Liên hệ đã được thêm vào thành công";
+                } catch(error){
+                    console.log(error);
+                }
+            },
+        },
+    }
+</script>
--- src/views/ContactEdit.vue
+++ src/views/ContactEdit.vue
@@ -15,56 +15,56 @@ import ContactService from "@/services/contact.service";
 
 export default {
     components: {
-        
+        ContactForm,
     },
     props: {
         id: { type: String, required: true },
     },
-data() {
-    return {
-        contact: null,
-        message: "",
-    };
-},
-methods: {
-    async getContact(id) {
-        try {
-            this.contact = await ContactService.get(id);
-        } catch (error) {
-            console.log(error);
-            // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
-            this.$router.push({
-            name: "notfound",
-            params: {
-                pathMatch: this.$route.path.split("/").slice(1)
-            },
-            query: this.$route.query,
-            hash: this.$route.hash,
-            });
-        }
+    data() {
+        return {
+            contact: null,
+            message: "",
+        };
     },
-    async updateContact(data) {
-        try {
-            await ContactService.update(this.contact._id, data);
-            this.message = "Liên hệ được cập nhật thành công.";
-        } catch (error) {
-            console.log(error);
-        }
-    },
-    async deleteContact() {
-        if (confirm("Bạn muốn xóa Liên hệ này?")) {
+    methods: {
+        async getContact(id) {
             try {
-                await ContactService.delete(this.contact._id);
-                this.$router.push({ name: "contactbook" });
+                this.contact = await ContactService.get(id);
+            } catch (error) {
+                console.log(error);
+                // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
+                this.$router.push({
+                name: "notfound",
+                params: {
+                    pathMatch: this.$route.path.split("/").slice(1)
+                },
+                query: this.$route.query,
+                hash: this.$route.hash,
+                });
+            }
+        },
+        async updateContact(data) {
+            try {
+                await ContactService.update(this.contact._id, data);
+                this.message = "Liên hệ được cập nhật thành công.";
             } catch (error) {
                 console.log(error);
             }
-        }
+        },
+        async deleteContact() {
+            if (confirm("Bạn muốn xóa Liên hệ này?")) {
+                try {
+                    await ContactService.delete(this.contact._id);
+                    this.$router.push({ name: "contactbook" });
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
     },
-},
-    created() {
-    this.getContact(this.id);
-    this.message = "";
-    },
-};
+        created() {
+        this.getContact(this.id);
+        this.message = "";
+        },
+    };
 </script>
